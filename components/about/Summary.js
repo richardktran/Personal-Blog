8975a1b5a45218cd8d181c26e827c64f57009087
Richard Annowit <richardannowit@gmail.com>
2022-03-01 23:13:42 +0700

    change the text of summary is stress text, not a blur text

--- components/about/Summary.js
+++ components/about/Summary.js
@@ -11,12 +11,12 @@ export default function Summary({ ...props }) {
     const textColor = useColorModeValue('light_text_stress', 'dark_text_stress')
 
     return (
-        <MotionFlex justify="center" align="center" direction="column" color={textColor}  {...props}>
+        <MotionFlex justify="center" align="center" direction="column" {...props}>
             <Heading as="h1" color={headingColor} textTransform='uppercase' fontWeight="bold">{trans.about.summary_title}</Heading>
-            <Text my={3} style={{ textIndent: 20 }}>
+            <Text my={3} style={{ textIndent: 20 }} color={textColor}>
                 {trans.about.summary1}
             </Text>
-            <Text style={{ textIndent: 20 }}>
+            <Text style={{ textIndent: 20 }} color={textColor}>
                 {trans.about.summary2}
             </Text>
         </MotionFlex>
